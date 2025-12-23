57b3fe2ab964a4676f659148dbdf88cccdc98715
Amysly <amarachukwuobiegue@gmail.com>
2025-12-23 15:17:09 +0100

    index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
-import React, { useEffect } from 'react';
-import { createBrowserRouter, 
-    createRoutesFromElements,
-     RouterProvider, 
-     Route } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import Homepage from './components/.Homepage';
-import MainLayout from './layouts/MainLayout';
-import About from './Pages/About';
-import Forms from './Pages/Forms';
-import Project from './Pages/Project';
-import NotFound from './Pages/NotFound';
-import Skills from './Pages/Skills';
-
+import Homepage from "./components/Homepage";
+import MainLayout from "./layouts/MainLayout";
+import About from "./Pages/About";
+import Forms from "./Pages/Forms";
+import Project from "./Pages/Project";
+import NotFound from "./Pages/NotFound";
+import Skills from "./Pages/Skills";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<Homepage />} />
-      <Route path='/about-me' element={<About />} />
-      <Route path ='/skills' element ={<Skills/>}/>
-      <Route path='/projects' element={<Project />} />
-      <Route path='/contact' element={<Forms />} />
-      <Route path='*' element={<NotFound />} />
-    </Route>
+    <>
+      {/* ROUTES WITH HEADER & FOOTER */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-me" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/contact" element={<Forms />} />
+      </Route>
+
+      {/* ROUTE WITHOUT HEADER & FOOTER */}
+      <Route path="*" element={<NotFound />} />
+    </>
   )
 );
 
 function App() {
   useEffect(() => {
     AOS.init({
-      duration: 2000
+      duration: 2000,
     });
   }, []);
 
